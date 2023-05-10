d06b8d4ee9083b2835c300152a2ecd59232f53dc
Anyaspb <124302415+Anyaspb@users.noreply.github.com>
2023-05-10 21:25:50 +0300

    Update mainORM.py

--- mainORM.py
+++ mainORM.py
@@ -1,6 +1,7 @@
 import sqlalchemy
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+import datetime
 
 Base = declarative_base()
 
@@ -30,7 +31,7 @@ class Stock(Base):
     __tablename__ = "stock"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    count = sq.Column(sq.Integer)
+    count = sq.Column(sq.String(length=40))
     id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
     id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
 
@@ -41,8 +42,8 @@ class Sale(Base):
     __tablename__ = "sale"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    count = sq.Column(sq.Integer)
-    price = sq.Column(sq.Integer)
+    count = sq.Column(sq.String(length=40))
+    price = sq.Column(sq.String(length=40))
     date_sale = sq.Column(sq.String(length=40))
     id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
 
@@ -71,15 +72,46 @@ engine = sqlalchemy.create_engine(DSN)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-requested_publisher = input("Укажите издателя:")
+def get_shops(requested_publisher):
+    if requested_publisher.isdigit(): #Проверяем переданные данные в функцию на то, что строка состоит только из чисел
+        q = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).select_from(Book).join(Stock).join(Publisher).join(Shop).join(Sale).filter(Publisher.id == requested_publisher).all()
+    else:
+        q = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).select_from(Book).join(Stock).join(Publisher).join(Shop).join(Sale).filter(Publisher.name == requested_publisher).all()
+    for s in q:
+        print(f"{s[0]: <40}  | {s[1]: <10} | {s[2]:<8} | { datetime.datetime.fromisoformat(s[3]).strftime('%Y-%m-%d')}")
+    session.close()
 
-q = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale).filter(Publisher.name.like(requested_publisher))
-# 1 вариант:
-print(q)
-print([[s[0]] + [s[1]] + [str(s[2])] + [str(s[3])] for s in q.all()])
-# 2 вариант:
-for s in q.all():
-    print(f'{s[0]} | {s[1]} | {s[2]} | {s[3]}')
+if __name__ == '__main__':
+    input_publisher = input("Укажите издателя: ") #Просим клиента ввести имя или айди публициста и данные сохраняем в переменную
+    get_shops(input_publisher) #Вызываем функцию получения данных из базы, передавая в функцию данные, которые ввел пользователь строкой выше
 
-session.close()
-
+# # Задание 3. Импорт из Json
+#
+# import json
+#
+# import sqlalchemy
+# from sqlalchemy.orm import sessionmaker
+#
+#
+# DSN = 'postgresql://postgres:postgres@localhost:5432/homework_db'
+# engine = sqlalchemy.create_engine(DSN)
+#
+#
+# Session = sessionmaker(bind=engine)
+# session = Session()
+#
+# with open('test_data.json', 'r') as fd:
+#     data = json.load(fd)
+#
+# for record in data:
+#     model = {
+#         'publisher': Publisher,
+#         'shop': Shop,
+#         'book': Book,
+#         'stock': Stock,
+#         'sale': Sale,
+#     }[record.get('model')]
+#     session.add(model(id=record.get('pk'), **record.get('fields')))
+# session.commit()
+# session.close()
+#
